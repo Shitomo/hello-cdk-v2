956a2baf02f662319a397c911247cea2d65f1a21
Shitomo <c.adaeibf@icloud.com>
2022-06-19 21:01:09 +0900

    first commit

--- lib/hello-cdk-v2-stack.ts
+++ lib/hello-cdk-v2-stack.ts
@@ -1,10 +1,32 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  RemovalPolicy,
+  Duration,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkV2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // バケット
+    new s3.Bucket(this, "S3Bucket", {
+      bucketName: "sample-backet-from-typescript",
+      // Bucketへの直接アクセスを禁止
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      // CDK Stack削除時にBucketも削除する
+      removalPolicy: RemovalPolicy.DESTROY,
+      //
+      lifecycleRules: [
+        {
+          enabled: true,
+          expiration: Duration.days(1),
+        },
+      ],
+    });
 
     // The code that defines your stack goes here
 
