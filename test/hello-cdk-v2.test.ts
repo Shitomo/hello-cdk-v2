a75fbda758252752a24fbfdb6cd07173c574d5fc
Shitomo <c.adaeibf@icloud.com>
2022-06-19 20:40:50 +0900

    Initial commit

---
+++ test/hello-cdk-v2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HelloCdkV2 from '../lib/hello-cdk-v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hello-cdk-v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HelloCdkV2.HelloCdkV2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
